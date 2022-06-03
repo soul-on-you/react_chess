a4814491428b50b6efdee5cdb503de8642e681b4
brendan <superden081@gmail.com>
2022-06-03 10:56:59 +0300

    added board and cell logic

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,19 +1,86 @@
 import { Cell } from "./Cell";
 import { Colors } from "./Colors";
+import { Bishop } from "./figures/Bishop";
+import { Horse } from "./figures/Horse";
+import { King } from "./figures/King";
+import { Knight } from "./figures/Kinght";
+import { Queen } from "./figures/Queen";
+import { Tower } from "./figures/Tower";
 
 export class Board {
-   cells: Cell[][] = [];
+  cells: Cell[][] = [];
 
   public initCells() {
     for (let i = 0; i < 8; i++) {
       const row: Cell[] = [];
       for (let j = 0; j < 8; j++) {
         if ((i + j) % 2)
-          row.push(new Cell(j, i, Colors.WHITE, null)); // добавляем белые
-        else row.push(new Cell(j, i, Colors.BLACK, null)); // добавляем черные
+          row.push(new Cell(this, j, i, Colors.WHITE, null)); // добавляем белые
+        else row.push(new Cell(this, j, i, Colors.BLACK, null)); // добавляем черные
       }
 
       this.cells.push(row);
     }
+  }
+
+  public getCell(x: number, y: number): Cell {
+    if (x < 8 && y < 8) return this.cells[y][x];
+    throw new Error("Can't get figure, out of board range");
+  }
+
+  public initFigures() {
+    this.addKnights();
+    this.addTowers();
+    this.addHorses();
+    this.addBishops();
+    this.addQueens();
+    this.addKings();
+  }
+
+  public highlightCells(selectedCell: Cell | null): void {
+    // if (selectedCell?.figure)
+    for (let i = 0; i < this.cells.length; i++) {
+      const row = this.cells[i];
+      for (let j = 0; j < row.length; j++) {
+        row[j].available = !!selectedCell?.figure?.canMove(row[j]);
+      }
+    }
+  }
+
+  public getCopy(this: Board): Board {
+    const newBoard = new Board();
+    newBoard.cells = this.cells;
+    return newBoard;
+  }
+
+  private addKnights(): void {
+    for (let i = 0; i < 8; i++) new Knight(this.getCell(i, 1), Colors.WHITE);
+    for (let i = 0; i < 8; i++) new Knight(this.getCell(i, 6), Colors.BLACK);
+  }
+  private addTowers(): void {
+    new Tower(this.getCell(0, 0), Colors.WHITE);
+    new Tower(this.getCell(7, 0), Colors.WHITE);
+    new Tower(this.getCell(0, 7), Colors.BLACK);
+    new Tower(this.getCell(7, 7), Colors.BLACK);
+  }
+  private addHorses(): void {
+    new Horse(this.getCell(1, 0), Colors.WHITE);
+    new Horse(this.getCell(6, 0), Colors.WHITE);
+    new Horse(this.getCell(1, 7), Colors.BLACK);
+    new Horse(this.getCell(6, 7), Colors.BLACK);
+  }
+  private addBishops(): void {
+    new Bishop(this.getCell(2, 0), Colors.WHITE);
+    new Bishop(this.getCell(5, 0), Colors.WHITE);
+    new Bishop(this.getCell(2, 7), Colors.BLACK);
+    new Bishop(this.getCell(5, 7), Colors.BLACK);
+  }
+  private addQueens(): void {
+    new Queen(this.getCell(3, 0), Colors.WHITE);
+    new Queen(this.getCell(4, 7), Colors.BLACK);
+  }
+  private addKings(): void {
+    new King(this.getCell(4, 0), Colors.WHITE);
+    new King(this.getCell(3, 7), Colors.BLACK);
   }
 }
--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -1,21 +1,81 @@
 import { nanoid } from "nanoid";
+import { Board } from "./Board";
 import { Colors } from "./Colors";
 import { IFigure } from "./figures/IFigure";
 
 export class Cell {
+  private readonly _board: Board;
   readonly x: number;
   readonly y: number;
   readonly color: Colors;
   figure: IFigure | null;
-  available:boolean ;
-  id:string;
+  available: boolean;
+  id: string;
 
-  constructor(x:number, y:number, color: Colors, figure: IFigure|null) {
-      this.id = nanoid(10);
-      this.x = x;
-      this.y = y;
-      this.color = color;
-      this.figure = figure; 
-      this.available = false;
+  constructor(
+    board: Board,
+    x: number,
+    y: number,
+    color: Colors,
+    figure: IFigure | null
+  ) {
+    this._board = board;
+    this.id = nanoid(10);
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.figure = figure;
+    this.available = false;
+  }
+
+  public moveFigure(target: Cell) {
+    if (this.figure && this.figure.canMove(target)) {
+      this.figure?.moveFigure(target);
+      target.figure = this.figure;
+      target.figure.cell = target;
+      this.figure = null;
+    }
+  }
+
+  isEmpty(): boolean {
+    return this.figure === null;
+  }
+
+  public isEmptyVertical(target: Cell): boolean {
+    if (this.x !== target.x) return false;
+
+    const min = Math.min(this.y, target.y);
+    const max = Math.max(this.y, target.y);
+
+    for (let y = min + 1; y < max; y++)
+      if (!this._board.getCell(this.x, y).isEmpty()) return false;
+
+    return true;
+  }
+  public isEmptyHorizontal(target: Cell): boolean {
+    if (this.y !== target.y) return false;
+
+    const min = Math.min(this.x, target.x);
+    const max = Math.max(this.x, target.x);
+
+    for (let x = min + 1; x < max; x++)
+      if (!this._board.getCell(x, this.y).isEmpty()) return false;
+
+    return true;
+  }
+  public isEmptyDiagonal(target: Cell): boolean {
+    const x = Math.abs(target.x - this.x);
+    const y = Math.abs(target.y - this.y);
+
+    if (x !== y) return false;
+
+    const dx = this.x < target.x ? 1 : -1;
+    const dy = this.y < target.y ? 1 : -1;
+
+    for (let i = 1; i < y; i++)
+      if (!this._board.getCell(this.x + dx * i, this.y + dy * i).isEmpty())
+        return false;
+
+    return true;
   }
 }
